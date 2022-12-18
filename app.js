c8f51af2bf60e6ee21f2e6094ae3cfadd7007166
FurkanBey123 <furkanboran123@gmail.com>
2022-12-18 13:36:55 +0300

    Security update

--- app.js
+++ app.js
@@ -3,8 +3,21 @@ const ejs = require("ejs");
 const biolink = require(__dirname + "/biolink.js");
 const app = express();
 
+const helmet = require('helmet');
+const cors = require('cors');
+const xss = require('xss-clean');
+const rateLimiter = require('express-rate-limit');
+
 app.set('view engine', 'ejs');
 app.use(express.static("public"));
+app.set('trust proxy', 1)
+app.use(rateLimiter({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 300 // limit each IP to 300 requests per windowMs
+}))
+app.use(helmet())
+app.use(cors())
+app.use(xss())
 
 app.get("/", function(req, res) {
   const captions = biolink.getBio(function(captions){
